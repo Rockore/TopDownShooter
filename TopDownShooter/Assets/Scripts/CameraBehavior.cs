8b6163182f0ec4e8a6491e4b436582f752bb188f
Noah Eis <108202164+Rockore@users.noreply.github.com>
2022-10-19 11:29:14 -0500

    Fix Camera

--- TopDownShooter/Assets/Scripts/CameraBehavior.cs
+++ TopDownShooter/Assets/Scripts/CameraBehavior.cs
@@ -19,23 +19,6 @@ public class CameraBehavior : MonoBehaviour
 
     private void ClampToRoom(object source, GoThroughDoorArgs args)
     {
-        cam.transform.position = new Vector3(PlayerMovement.playerRoomPosition.x * 20, PlayerMovement.playerRoomPosition.y * 15);
-        /*switch (args.Direction)
-        {
-            case "TopDoor":
-                cam.transform.position = new Vector3(PlayerMovement.playerRoomPosition.x, PlayerMovement.playerRoomPosition.y * 15);
-                break;
-            case "RightDoor":
-                cam.transform.position = new Vector3(PlayerMovement.playerRoomPosition.x * 20, PlayerMovement.playerRoomPosition.y);
-                break;
-            case "BottomDoor":
-                cam.transform.position = new Vector3(PlayerMovement.playerRoomPosition.x, PlayerMovement.playerRoomPosition.y * -15);
-                break;
-            case "LeftDoor":
-                cam.transform.position = new Vector3(PlayerMovement.playerRoomPosition.x * -20, PlayerMovement.playerRoomPosition.y);
-                break;
-            default:
-                return;
-        }*/
+        cam.transform.position = new Vector3(PlayerMovement.playerRoomPosition.x * 20, PlayerMovement.playerRoomPosition.y * 15, -10);
     }
 }
